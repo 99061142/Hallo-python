c4ba6a4e718871d88f19a78a7b95ed06773b9c9d
99061142 <99061142@mydavinci.nl>
2021-09-24 13:32:39 +0200

    Code + Image what the code shows

---
+++ hello.py
@@ -0,0 +1,16 @@
+print(  
+    "Hello wereld van…"
+
+    "\n\n"
+
+    "pppp    y     y   ttttttt   h   h    oooo   nnnn \n"     
+    "p   p    y   y       t      h   h   o    o  n   n \n"
+    "p   p     y y        t      h   h   o    o  n   n \n"
+    "pppp       y         t      hhhhh   o    o  n   n \n"
+    "p          y         t      h   h   o    o  n   n \n"
+    "p          y         t      h   h   o    o  n   n \n"
+    "p          y         t      h   h    oooo   n   n \n"
+)
+
+# Xander Briem
+# 99061142
